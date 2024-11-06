72aaafffbb5e40d01726b9086d83ad25475995f9
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2024-11-06 21:07:35 +0100

    Exercise published

---
+++ src/Pacchetto1/Main.java
@@ -0,0 +1,11 @@
+package Pacchetto1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SchedaPersona schedaPersona = new SchedaPersona("Matteo Ruggieri", "Via di test, 15", "3279998887");
+		
+		System.out.println(schedaPersona.toString());
+	}
+
+}
